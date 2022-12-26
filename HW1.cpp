2ff0e43a7bfc77612ef4307c50dc1e8b65ee0301
brmcbrid <69523108+brmcbrid@users.noreply.github.com>
2022-12-26 12:25:58 -0500

    Create HW1.cpp

---
+++ HW1.cpp
@@ -0,0 +1,15 @@
+// Workers at a particular company have won a 7.6% pay increase 
+// retroactive for six months.
+// Write a program that prompts a user for their full name 
+// and previous annual salary as inputs and then calculate 
+// and output a report of the amount of retroactive pay due 
+// the employee, the new annual salary, and the new monthly 
+// salary using the given format.
+// Complete the gien code for the main function
+
+#include <iostream>
+
+int main() {
+    
+    return 0;
+}
