ac3d0a8b728a340f694bdbc74b985b23f6396957
brmcbrid <69523108+brmcbrid@users.noreply.github.com>
2025-07-08 11:30:35 -0400

    Initial commit

--- Lab3.cpp
+++ Lab3.cpp
@@ -1,16 +1,7 @@
-// Workers at a particular company have won a 7.6% pay increase 
-// retroactive for six months.
-// Write a program that prompts a user for their 
-// previous annual salary then calculates
-// an output report of the amount of retroactive pay due 
-// the employee, the new annual salary, and the new monthly 
-// salary using the given format.
-// Complete the given code for the main function
-
 #include <iostream>
 using namespace std;
 
 int main() {
-    
+    // Place your solution code here for Lab3
     return 0;
 }
