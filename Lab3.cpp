170e2db9fb3003348bfdf2657276e7459b9248bf
aston <70747479+astonscode@users.noreply.github.com>
2025-09-10 13:53:09 -0400

    feat: it's alive!!!

--- Lab3.cpp
+++ Lab3.cpp
@@ -1,7 +1,22 @@
 #include <iostream>
-using namespace std;
+#include <iomanip>
 
-int main() {
-    // Place your solution code here for Lab3
-    return 0;
+int main()
+{
+   int base_sal;
+   const float INCREASE = 1.076;
+
+   std::cout << "Salary: $";
+   std::cin >> base_sal;
+
+   double adjusted_sal = base_sal * INCREASE;
+   double retro_pay = adjusted_sal - base_sal;
+   double monthly_sal = adjusted_sal / 12;
+
+   std::cout << std::fixed << std::setprecision(2);
+   std::cout << "New Salary at 7.6% incr: $" << adjusted_sal << "\n"
+             << "Retro pay: $" << retro_pay << "\n"
+             << "Monthly pay: $" << monthly_sal;
+
+   return 0;
 }
